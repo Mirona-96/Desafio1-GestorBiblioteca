ae3d1b5552c20047d38a2b59309f871050b36a94
HomePC <Eltonmirona10@gmail.com>
2025-03-26 13:54:59 +0200

    Classe Gerenciador actualizada

--- Gerenciador.cs
+++ Gerenciador.cs
@@ -14,14 +14,15 @@ namespace GestaoBiblioteca
         public List<Usuario> usuarios { get; } = [];
         public List<Emprestimo> emprestimos { get; } = [];
 
-       // LivroController livroController = new LivroController();
+        // LivroController livroController = new LivroController();
 
-        public void AdicionarLivros (Livro livro)
-        {    
+        public void AdicionarLivros(Livro livro)
+        {
             try
             {
                 livro.ValidarDados();
-            } catch (Exception erro)
+            }
+            catch (Exception erro)
             {
                 Console.WriteLine($"Erro: Dado invalido: {erro.Message}");
                 return;
@@ -34,19 +35,19 @@ namespace GestaoBiblioteca
             int index = 1;
             Console.WriteLine("Lista de Livros");
             Console.WriteLine("-", 50);
-            foreach(var livro in livros)
+            foreach (var livro in livros)
             {
                 Console.WriteLine($" Livro #{index}\n" +
-                    $"{"Codigo de Registo:", -20} {livro.IdLivro}\n" +
+                    $"{"Codigo de Registo:",-20} {livro.IdLivro}\n" +
                     $"{"Título:",-20}   {livro.Titulo}\n" +
-                    $"{"ISBN:",-20}   { livro.ISBN}\n" +
-                    $"{"Ano de Publicação:", -20}  {livro.AnoPublicacao}. ");
+                    $"{"ISBN:",-20}   {livro.ISBN}\n" +
+                    $"{"Ano de Publicação:",-20}  {livro.AnoPublicacao}. ");
             }
         }
 
         public void ConsultarUmLivro(string consulta)
         {
-            foreach(var livro in livros)
+            foreach (var livro in livros)
             {
                 if (livro.Titulo.Contains(consulta, StringComparison.OrdinalIgnoreCase) ||
                     livro.Autor.Contains(consulta, StringComparison.OrdinalIgnoreCase))
@@ -68,16 +69,27 @@ namespace GestaoBiblioteca
             usuarios.Add(usuario);
         }
 
-        public void AdicionarEmprestimo(Emprestimo emprestimo)
+        public void AdicionarEmprestimo(Emprestimo emprestimo, Livro livro, Usuario usuario)
         {
-//          emprestimo.AtribuirLivro();
+            emprestimo.AtribuirLivro(livro);
+            emprestimo.AtribuirUsuario(usuario);
             emprestimos.Add(emprestimo);
         }
 
         public void ConsultarEmprestimos()
         {
-
+            int index = 1;
+            Console.WriteLine("Lista de Emprestimos");
+            Console.WriteLine("-", 50);
+            foreach (var emprestimo in emprestimos)
+            {
+                Console.WriteLine($" Empréstimo #{index}\n" +
+                    $"{"Codigo de Empréstimo:",-20} {emprestimo.IdEmprestimo}\n" +
+                    $"{"Livro:",-20}   {emprestimo.Consultarlivro}\n" +
+                    $"{"Usuario:",-20}   {emprestimo.ConsultarUsuario}\n" +
+                    $"{"Data do Empréstimo:",-20}  {emprestimo.DataEmprestimo}\n" +
+                    $"{"Data da Devolução: ",-20} {emprestimo.DataDevolucao}");
+            }
         }
-
     }
 }
